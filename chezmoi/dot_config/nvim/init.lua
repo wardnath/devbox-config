85cf221106c596a66a43ff98fd0f266e2ca922c5
wardnath <wardnath.alt@gmail.com>
2024-08-23 14:14:37 +0000

    devbox commit

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -1,3 +1,4 @@
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -22,16 +23,58 @@ require('lazy').setup({
   {
     'huggingface/llm.nvim',
     opts = {
-      model = "tinyllama:latest",
+      model = "codegemma:2b-code-q2_K",
       url = "http://localhost:11434", -- llm-ls uses "/api/generate"
+      -- api_token = nil, -- cf Install paragraph
+      -- model = "bigcode/starcoder2-15b", -- the model ID, behavior depends on backend
+      -- backend = "huggingface", -- backend ID, "huggingface" | "ollama" | "openai" | "tgi"
+      -- url = nil, -- the http url of the backend
+      -- tokens_to_clear = { "<|endoftext|>" }, -- tokens to remove from the model's output
+      -- parameters that are added to the request body, values are arbitrary, you can set any field:value pair here it will be passed as is to the backend
+      -- request_body = {
+      --    parameters = {
+      --      max_new_tokens = 60,
+      --      temperature = 0.2,
+      --      top_p = 0.95,
+      --    },
+      --  },
+      -- set this if the model supports fill in the middle
+      fim = {
+        enabled = true,
+        prefix = "<fim_prefix>",
+        middle = "<fim_middle>",
+        suffix = "<fim_suffix>",
+      },
+      -- debounce_ms = 150,
+      -- accept_keymap = "<Tab>",
+      -- dismiss_keymap = "<S-Tab>",
+      -- tls_skip_verify_insecure = false,
+      -- llm-ls configuration, cf llm-ls section
+      l-- sp = {
+      --  bin_path = nil,
+      --  host = nil,
+      --  port = nil,
+      --cmd_env = nil, -- or { LLM_LOG_LEVEL = "DEBUG" } to set the log level of llm-ls
+      --version = "0.5.3",
+      --},
+      --tokenizer = nil, -- cf Tokenizer paragraph
+      --context_window = 1024, -- max number of tokens for the context window
+      --enable_suggestions_on_startup = true,
+      --enable_suggestions_on_files = "*", -- pattern matching syntax to enable suggestions on specific files, either a string or a list of strings
+      --disable_url_path_completion = false, -- cf Backend
       request_body = {
+	-- prompt=f'<|fim_prefix|>{prefix}<|fim_suffix|>{suffix}<|fim_middle|>',
         options = {
-          temperature = 0.2,
-          top_p = 0.95,
+          temperature = 0.01,
+          top_p = 0.9,
+	  num_predict: 128,
+          stop: ["<|file_separator|>"],
         }
       }
     }
   },
+  {
+  }
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },  -- Catppuccin theme
   {
     "folke/which-key.nvim",
@@ -101,3 +144,5 @@ require("catppuccin").setup({
 
 -- Apply the Catppuccin theme
 vim.cmd.colorscheme "catppuccin"
+
+
