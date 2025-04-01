ff1c5439781ba729bff6e31a7fcf17b957226aad
Nathan Lloyd Ward <wardnath.alt@gmail.com>
2025-04-01 18:17:23 -0400

    Update init.lua

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -166,8 +166,85 @@ vim.opt.listchars = {
 }
 
 require("lazy").setup({
-  -- REMOVE fast-cursor-move plugin entry since we embedded it directly above.
-
+ -- File Explorer that supplements but doesn't replace Netrw
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      -- Keep Netrw enabled
+      vim.g.loaded_netrw = 0
+      vim.g.loaded_netrwPlugin = 0
+      
+      require("nvim-tree").setup({
+        disable_netrw = false,       -- Don't disable Netrw
+        hijack_netrw = false,        -- Don't hijack Netrw windows
+        hijack_cursor = false,       -- Keep cursor behavior consistent
+        update_cwd = false,          -- Don't change working directory
+        actions = {
+          open_file = {
+            quit_on_open = true,     -- Close the tree when opening a file
+          },
+        },
+        view = {
+          width = 30,
+          side = "left",
+        },
+        renderer = {
+          group_empty = true,        -- Compact folders that contain only a single folder
+        },
+      })
+      
+      -- Use different keymaps for Netrw and nvim-tree to avoid confusion
+      vim.keymap.set('n', '<leader>e', '<cmd>Lexplore<CR>', { desc = 'Toggle Netrw file explorer' })
+      vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
+    end
+  },
+  -- Telescope - fuzzy finder that enhances but doesn't replace built-in search
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git/" },
+          path_display = { "truncate" }
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case"
+          }
+        }
+      })
+      
+      -- Load fzf extension if available
+      pcall(function() require("telescope").load_extension("fzf") end)
+      
+      -- Key mappings that coexist with built-in search
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Help tags" })
+    end
+  },
+  -- Comment plugin with intuitive keybindings
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup({
+        -- Use familiar gc operator (similar to built-in formatting operators)
+        toggler = { line = 'gcc', block = 'gbc' },
+        opleader = { line = 'gc', block = 'gb' },
+      })
+    end
+  },
   {
     "ggandor/leap.nvim",
     event = "VeryLazy",
